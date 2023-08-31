d0a8e368271873292e5564657ff38310f9d30c47
梁文杰 <1139629972@qq.com>
2023-08-31 11:35:59 +0800

    add: 添加初始化默认用户功能

--- backend/internal/app/initialize/gorm.go
+++ backend/internal/app/initialize/gorm.go
@@ -3,6 +3,7 @@ package initialize
 import (
 	"backend/internal/app/global"
 	"backend/internal/app/model"
+	uuid "github.com/satori/go.uuid"
 	"os"
 
 	"go.uber.org/zap"
@@ -15,6 +16,7 @@ func InitCommonDB() {
 	if db != nil {
 		global.DB = db
 		RegisterTables(db) // 初始化表
+		InitUser(db)       // 初始化默认用户
 	}
 }
 
@@ -40,4 +42,30 @@ func RegisterTables(db *gorm.DB) {
 		os.Exit(0)
 	}
 	global.LOG.Info("register table success")
+
+}
+
+// InitUser 初始化默认账户
+func InitUser(db *gorm.DB) {
+	// 判断是否存在用户
+	var count int64
+	if err := db.Model(&model.User{}).Count(&count).Error; err != nil {
+		global.LOG.Error("init user failed", zap.Error(err))
+		os.Exit(0)
+	}
+	if count > 0 {
+		return
+	}
+	// 创建默认用户
+	user := model.User{
+		UUID:        uuid.NewV4(),
+		Username:    "root",
+		Password:    "$2a$10$7iTyC7BlYSofctYy6.6bq.sL12FfybC/hZQ5K/5lqhuINjAAGSSw2",
+		Nickname:    "root",
+		AuthorityId: "999",
+	}
+	if err := db.Create(&user).Error; err != nil {
+		global.LOG.Error("create init user failed", zap.Error(err))
+		os.Exit(0)
+	}
 }
