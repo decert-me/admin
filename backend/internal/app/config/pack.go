a35d7797acf04aa7b16a1e9dcec46987ac8ddf2b
梁文杰 <1139629972@qq.com>
2023-09-05 14:38:32 +0800

    en: 分离打包模块

--- backend/internal/app/config/pack.go
+++ backend/internal/app/config/pack.go
@@ -1,6 +1,6 @@
 package config
 
 type Pack struct {
-	Path        string `mapstructure:"path" json:"path" yaml:"path"`                         // 项目路径
+	Server      string `mapstructure:"server" json:"server" yaml:"server"`                   // 打包服务
 	PublishPath string `mapstructure:"publish-path" json:"publish-path" yaml:"publish-path"` // 发布项目路径
 }
