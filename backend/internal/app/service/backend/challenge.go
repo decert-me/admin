5b4ad3b866488015a0267fcfe4a997c936063e1e
梁文杰 <1139629972@qq.com>
2024-01-10 17:45:08 +0800

    fix: 开放题提交时间

--- backend/internal/app/service/backend/challenge.go
+++ backend/internal/app/service/backend/challenge.go
@@ -121,7 +121,7 @@ func GetUserOpenQuestListV2(r request.GetUserOpenQuestListRequest) (list []respo
 						ELSE 1
 					END AS open_quest_review_status,
 					json_element->>'open_quest_review_time' AS open_quest_review_time,
-					user_open_quest.updated_at,
+					COALESCE(user_open_quest.commit_time,user_open_quest.updated_at) as updated_at,
 					(idx::int - 1)  AS index,
 					json_element->>'type' AS type,
 					json_element->>'value' AS value,
@@ -172,7 +172,7 @@ func GetUserOpenQuestListV2(r request.GetUserOpenQuestListRequest) (list []respo
 						ELSE 1
 					END AS open_quest_review_status,
 					json_element->>'open_quest_review_time' AS open_quest_review_time,
-					user_open_quest.updated_at,
+					COALESCE(user_open_quest.commit_time,user_open_quest.updated_at) as updated_at,
 					(idx::int - 1) AS index,
 					json_element->>'type' AS type,
 					json_element->>'value' AS value,
@@ -205,7 +205,7 @@ func ReviewOpenQuestV2(req []request.ReviewOpenQuestRequestV2) (err error) {
 	questMap := make(map[int64]model.Quest)
 	for _, r := range req {
 		var userOpenQuest model.UserOpenQuest
-		if err = db.Model(&model.UserOpenQuest{}).Where("id = ? AND open_quest_review_status = 1", r.ID).First(&userOpenQuest).Error; err != nil {
+		if err = db.Model(&model.UserOpenQuest{}).Select("*,COALESCE(user_open_quest.commit_time,user_open_quest.updated_at) as updated_at").Where("id = ? AND open_quest_review_status = 1", r.ID).First(&userOpenQuest).Error; err != nil {
 			db.Rollback()
 			return errors.New("该回答已经评分，请勿重复评分")
 		}
