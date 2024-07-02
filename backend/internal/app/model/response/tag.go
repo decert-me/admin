22c3e95c3f4e287099d11d2e78634d7cf217b3a9
liangjies <1139629972@qq.com>
2024-07-02 13:59:18 +0800

    fix: 标签用户列表昵称显示

--- backend/internal/app/model/response/tag.go
+++ backend/internal/app/model/response/tag.go
@@ -13,6 +13,6 @@ type GetTagListRes struct {
 type GetTagUserListRes struct {
 	ID        uint      `gorm:"primarykey" json:"id"` // 主键ID
 	CreatedAt time.Time `json:"createdAt"`            // 创建时间
-	NickName  string    `gorm:"column:nickname;type:varchar(200);default:''" json:"nickname" form:"nickname"`
+	NickName  string    `gorm:"column:name;type:varchar(200);default:''" json:"nickname" form:"nickname"`
 	Address   string    `json:"address" gorm:"comment:钱包地址;unique"`
 }
