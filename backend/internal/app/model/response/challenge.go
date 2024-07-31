d70fd27c2a1ec224dbf007f98e8607b4d73346de
liangjies <1139629972@qq.com>
2024-07-31 23:22:15 +0800

    fix: 评分的提交时间错误

--- backend/internal/app/model/response/challenge.go
+++ backend/internal/app/model/response/challenge.go
@@ -36,6 +36,7 @@ type GetUserOpenQuestDetailListV2 struct {
 	OpenQuestReviewStatus uint8          `gorm:"column:open_quest_review_status;default:0;comment:评阅开放题状态 1 未审核 2 已审核" json:"open_quest_review_status" form:"open_quest_review_status"` // // 评阅开放题状态 1 未审核 2 已审核
 	OpenQuestReviewTime   string         `gorm:"column:open_quest_review_time;comment:评阅开放题时间" json:"open_quest_review_time" form:"open_quest_review_time"`
 	UpdatedAt             time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	CreatedAt             time.Time      `gorm:"column:created_at" json:"created_at"`
 	Index                 int            `gorm:"column:index" json:"index"`
 	Title                 string         `gorm:"column:title" json:"title"`
 	Answer                datatypes.JSON `gorm:"column:answer" json:"answer"`
