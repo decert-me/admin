9529bc9bdc3f8443e8d8e9504f9d41e66b2c79d9
liangjies <1139629972@qq.com>
2024-08-06 10:09:53 +0800

    fix: 过滤未启用挑战

--- backend/internal/app/service/backend/statistics.go
+++ backend/internal/app/service/backend/statistics.go
@@ -71,7 +71,7 @@ func GetChallengeStatistics(r request.GetChallengeStatisticsReq) (res []response
 		whereList = append(whereList, fmt.Sprintf("(users.address ILIKE ? OR users.name ILIKE ?)"))
 		valueList = append(valueList, "%"+r.SearchAddress+"%", "%"+r.SearchAddress+"%")
 	}
-	dataSQL += " WHERE quest.token_id is not null AND  users.address is not null AND quest.disable = false"
+	dataSQL += " WHERE quest.token_id is not null AND  users.address is not null AND quest.disabled = false"
 	if len(whereList) > 0 {
 		dataSQL += " AND " + strings.Join(whereList, " AND ")
 	}
